072cf526b103129965add7e198addb9a2d45d4c5
ellabisk <ellabisk@gmail.com>
2025-08-25 21:16:46 +0200

    Add files via upload

---
+++ .js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <title>Pagina Iniziale</title>
+</head>
+<body>
+  <h1>Benvenuto!</h1>
+  <a href="robot.html">Clicca qui per vedere il robottino che balla e canta 🎶🤖</a>
+</body>
+</html>
